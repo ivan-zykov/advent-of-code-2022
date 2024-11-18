54c6c0e075ed7ecef3d0c1905173d0ea599c6cf1
Ivan Zykov <ivan.zykov@erstegroup.com>
2024-11-18 18:25:02 +0100

    Refactor part 1 to reading input as ling string

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,10 @@
+import java.io.File
+
 fun main() {
+
+    fun part1Alt(input: String): Int =
+        input.toMatrixAlt()
+            .maxOf { it.sum() }
 
     fun part1(input: List<String>): Int =
         input.toMatrix()
@@ -12,13 +18,26 @@ fun main() {
             .sum()
 
     // test if implementation meets criteria from the description, like:
+    val testInputAlt: String = File("src/Day01_test.txt").readText()
+    check(part1Alt(testInputAlt) == 24000)
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 333)
+    check(part1(testInput) == 24000)
+
+    val inputAlt: String = File("src/Day01.txt").readText()
+    check(part1Alt(inputAlt) == 65912) // Correct: 65912
 
     val input = readInput("Day01")
     check(part1(input) == 65912) // Correct: 65912
     check(part2(input) == 195625) // Correct: 195625
 }
+
+fun String.toMatrixAlt() = split("\n\n")
+    .map { elf ->
+        elf.lines()
+            .filter { it.isNotBlank() }
+            .map { itemString -> itemString.toInt() }
+    }
 
 fun List<String>.toMatrix(): List<List<Int>> {
     val matrix: MutableList<List<Int>> = mutableListOf()
